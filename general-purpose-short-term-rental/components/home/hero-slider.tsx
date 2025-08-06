a03abdfd0a3cbfb922a96f08c52903ecad56d97e
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-06 20:00:42 +0300

    add swiper to homepage

---
+++ general-purpose-short-term-rental/components/home/hero-slider.tsx
@@ -0,0 +1,35 @@
+"use client"; // <===== REQUIRED
+
+import React from "react";
+
+// Swiper components, modules and styles
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export default function App() {
+  return (
+    <>
+      <Swiper 
+        navigation={true}
+        pagination={{ type: "bullets", clickable: true }}
+        autoplay={true}
+        loop={true}
+        modules={[Autoplay, Navigation, Pagination]}
+        className="homepage-hero-swiper" style={{height: "100vh"}}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
