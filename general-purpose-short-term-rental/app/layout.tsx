ab8f6744427660c068eaeee7e1f916c151c0c524
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-06 19:59:12 +0300

    Create basic layout

--- general-purpose-short-term-rental/app/layout.tsx
+++ general-purpose-short-term-rental/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import "../css/homepage.css";
+
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
