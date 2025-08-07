88dbd086544902e5bcdcfa3d3a427b96ebc5513a
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-07 20:25:57 +0300

    added sonner

--- general-purpose-short-term-rental/app/layout.tsx
+++ general-purpose-short-term-rental/app/layout.tsx
@@ -7,6 +7,8 @@ import "../css/homepage.css";
 import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
 
+import { Toaster } from "@/components/ui/sonner"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -35,6 +37,7 @@ export default function RootLayout({
         <Header/>
         {children}
         <Footer/>
+        <Toaster />
       </body>
     </html>
   );
