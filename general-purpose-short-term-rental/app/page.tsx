3268cb53650cf47dcbc633267f8ea641d9289c1f
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-07 20:26:18 +0300

    added homepage tabs (WIP)

--- general-purpose-short-term-rental/app/page.tsx
+++ general-purpose-short-term-rental/app/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import HeroSlider from "@/components/home/hero-slider";
 import FeaturedListings from "@/components/home/featured-listings";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -14,6 +16,18 @@ export default function Home() {
 
         <div className="all-listings">
             <FeaturedListings />
+        </div>
+
+        <div className="homepage-tabs">
+          <Tabs defaultValue="city" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="city">City properties</TabsTrigger>
+              <TabsTrigger value="quiet">Quiet Properties</TabsTrigger>
+            </TabsList>
+            <TabsContent value="city">Todo: fill with swiper content</TabsContent>
+            <TabsContent value="quiet">Todo: fill with different swiper content</TabsContent>
+          </Tabs>
+          
         </div>
       </main>
     </div>
