d4364b461bd379b0ac434e4daae3bd5708dd8818
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-11 20:16:39 +0300

    new api route to get property by slug

---
+++ app/api/properties/propertyBySlug/[slug]/route.ts
@@ -0,0 +1,11 @@
+import { db } from "@/db/drizzle";
+import { property } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, context: { params: Promise<{ slug: string }> }) {
+    console.log("fetching begins");
+    const { slug } = await context.params;
+    const propertyData = await db.select().from(property).where(eq(property.slug, slug));
+    return NextResponse.json(propertyData.length ? propertyData[0] : null);
+}
