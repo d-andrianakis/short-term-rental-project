d38d06f3abfa96a364bbc73dd024531493ea5c4a
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-11 20:22:12 +0300

    generate property meta title dynamically

--- app/properties/property/[slug]/page.tsx
+++ app/properties/property/[slug]/page.tsx
@@ -10,6 +10,26 @@ import { notFound } from 'next/navigation';
 
 import PropertyPageClient from "@/components/properties/property/propertyPageClient";
 
+import { Metadata } from 'next';
+
+export async function generateMetadata(
+  { params }: { params: { slug: string } }
+): Promise<Metadata> {
+  const awaitedParams = await params;
+  const slugParam = awaitedParams.slug;
+
+  const propertyData = await db.select().from(property).where(eq(property.slug, slugParam));
+
+  return {
+    title: propertyData[0].name, // Dynamic title
+    description: propertyData[0].name, // Optional meta description
+    openGraph: {
+      title: propertyData[0].name,
+      description: propertyData[0].name,
+    },
+  }
+}
+
 export default async function PropertyPage({
   params,
 }: {
