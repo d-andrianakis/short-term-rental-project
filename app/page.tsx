0679d611b033beae4f3a9ea14c30c16b1ac91505
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-12 19:42:50 +0300

    container fixes and spacing

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-6 gap-16 sm:py-10">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start max-w-[1600px] w-full">
 
         <div className="w-full">
