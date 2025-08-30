ecb17fafb6d32890ceb6b653c103d1951b79dbcc
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-30 09:48:19 +0300

    datetime picker + shadcn components for it

--- components/common/search.tsx
+++ components/common/search.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import { DateTimePicker24h } from "@/components/ui/datetime-picker-24h";
 
 import { useQueryState } from "nuqs";
 
@@ -8,6 +9,11 @@ export default function Search() {
     const [city, setCity] = useQueryState("city", { defaultValue: "" });
 
     return (
-        <Input type="text" value={city} onChange={(e) => setCity(e.target.value || null)} placeholder="Search..." className="w-full max-w-md" />
+        <>
+            <div className="w-fit flex">
+                <Input type="text" value={city} onChange={(e) => setCity(e.target.value || null)} placeholder="Search..." className="min-w-52 max-w-md" />
+                <DateTimePicker24h />
+            </div>
+        </>  
     )
 }
