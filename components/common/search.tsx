2fd9e8fcd2f5c1924a70f4769b3c11a272ce626e
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-08-30 15:16:53 +0300

    Streamline search component

--- components/common/search.tsx
+++ components/common/search.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
-import { DateTimePicker24hForm } from "@/components/ui/datetime-picker-24h";
-
-import { useQueryState } from "nuqs";
+import { SearchForm } from "@/components/ui/search-form";
 
 export default function Search() {
-    const [city, setCity] = useQueryState("city", { defaultValue: "" });
-
     return (
         <>
             <div className="w-fit flex">
-                <Input type="text" value={city} onChange={(e) => setCity(e.target.value || null)} placeholder="Search..." className="min-w-52 max-w-md" />
-                <DateTimePicker24hForm />
+                <SearchForm />
             </div>
         </>  
     )
