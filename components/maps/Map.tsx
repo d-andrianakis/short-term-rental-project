acf7802591b0da6f9844ac55283ae920d7aeacd9
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-10-11 17:27:32 +0300

    - @wip: map incremental progress

--- components/maps/Map.tsx
+++ components/maps/Map.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import {
+  Marker,
+  APIProvider,
+  ControlPosition,
+  Map,
+  MapControl,
+  useMap
+} from '@vis.gl/react-google-maps';
+
 import { useMemo } from 'react';
 
 type Location = {
@@ -17,15 +25,25 @@ export default function GoogleMapComponent({ locations }: GoogleMapProps) {
   // Default map center (you can also compute dynamically)
   const center = useMemo(() => locations[0] ?? { lat: 37.7749, lng: -122.4194 }, [locations]);
 
+  const map = useMap();
+
+  console.log(map?.getBounds());
+
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ''}>
-      <div style={{ height: '400px', width: '100%', borderRadius: '12px', overflow: 'hidden' }}>
+      <div style={{ height: '400px', width: '100%', borderRadius: '12px', overflow: 'hidden', marginBottom: 20 }}>
         <Map
           zoom={10}
           center={center}
           gestureHandling={'greedy'}
           disableDefaultUI={false}
         >
+          <MapControl position={ControlPosition.TOP_RIGHT}>
+            <div style={{background: 'white', color: 'black', padding:8}}>
+              .. any component here will be added to the control-containers of the
+          google map instance ..
+            </div>
+          </MapControl>
           {locations.map((loc, i) => (
             <Marker
               key={i}
