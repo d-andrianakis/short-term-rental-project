939e1254edc1977f25589f97030ff4f12ff3f55d
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-10-18 16:27:01 +0300

    - @wip: progress in property page

--- components/properties/property/propertyPageClient.tsx
+++ components/properties/property/propertyPageClient.tsx
@@ -1,9 +1,19 @@
 'use client';
 
 import Image from "next/image";
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 
+import { useTranslations } from 'next-intl';
+
 export default function PropertyPageClient({ property }) {
+  const g = useTranslations("Property");
+
+  const handleClick = () => {
+    console.log('clicked');
+  }
+
   return (
     <main className="flex space-x-5">
       <div className="w-3/4">
@@ -13,18 +23,25 @@ export default function PropertyPageClient({ property }) {
           width={0}
           height={0}
           sizes="100vw"
-          style={{ width: '100%', height: 'auto' }}
-          className="rounded-xl"
+          className="rounded-xl w-full h-auto"
         />
         <div>
           <h1>{property.name}</h1>
           <p>{property.slug}</p>
         </div>
       </div>
-      <aside className="w-1/4">
-        <div>
-          <Button variant="secondary">
-            Book now!
+      <aside className="w-1/4 h-screen relative">
+        <div className="sticky top-0">
+          <Button
+            variant="secondary"
+            className="w-full"
+            onClick={handleClick}
+          >
+            <Link
+              href="/checkout/checkout"
+            >
+              {g('book_now')}
+            </Link>
           </Button>
         </div>
       </aside>
