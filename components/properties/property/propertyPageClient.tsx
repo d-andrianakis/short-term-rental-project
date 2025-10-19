f437886c2260afce3912916436ed52028c93cd06
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-10-19 15:16:36 +0300

    - @feat: handle property review scores

--- components/properties/property/propertyPageClient.tsx
+++ components/properties/property/propertyPageClient.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { useTranslations } from 'next-intl';
 
 import { usePropertyStore } from "@/store/usePropertyStore";
+import { useEffect, useState } from "react";
 
 export default function PropertyPageClient({ property }) {
   const router = useRouter();
@@ -15,11 +16,45 @@ export default function PropertyPageClient({ property }) {
 
   const g = useTranslations("Property");
 
-  const handleClick = () => {
-    setPropertyId(property.id);
+  const [reviewScore, setReviewScore] = useState<number | null>(null);
+  const [loadingScore, setLoadingScore] = useState(true);
 
+  const handleClick = () => {
+    // handle setting peristent property data with Zustand first and then redirect to checkout
+    setPropertyId(property.id);
+    
     router.push('/checkout/checkout');
   };
+
+  async function fetchData() {
+    try {
+      const res = await fetch("/api/properties/getPropertyReviewScore", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(property.id),
+      });
+
+      const data = await res.json();
+
+      let score = '';
+      if (data) {
+        score = data ?? null;
+        score = parseFloat(data);
+        score = parseFloat(score.toFixed(2));
+      }      
+
+      setReviewScore(typeof score === 'number' ? score : null);
+    } catch (err) {
+      console.error("Failed to fetch review score", err);
+      setReviewScore(null);
+    } finally {
+      setLoadingScore(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   
   return (
     <main className="flex space-x-5">
@@ -32,6 +67,15 @@ export default function PropertyPageClient({ property }) {
           sizes="100vw"
           className="rounded-xl w-full h-auto"
         />
+        <div className="mt-3">
+          {loadingScore ? (
+            <p>Loading review score...</p>
+          ) : reviewScore !== null ? (
+            <p>{g('rating')} {reviewScore}</p>
+          ) : (
+            <p>No review score available</p>
+          )}
+        </div>
         <div>
           <h1>{property.name}</h1>
           <p>{property.slug}</p>
