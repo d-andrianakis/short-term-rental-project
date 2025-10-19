5b7dff444595adfedc4ec9f4e9bfc0d718e53f60
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-10-19 15:15:08 +0300

    - @feat: add api route for review score

---
+++ app/api/properties/getPropertyReviewScore/route.ts
@@ -0,0 +1,28 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+import { db } from "@/db/drizzle";
+import { property, property_reviews } from "@/db/schema";
+import { avg, eq } from 'drizzle-orm';
+import { numeric } from 'drizzle-orm/sqlite-core';
+ 
+export async function POST(request: NextRequest) {
+
+    const data = await request.json();
+
+    const propertyId: number[] = data;
+
+    if (propertyId) {
+        const query = db.select({ numeric: avg(property_reviews.overallScore) })
+        .from(property_reviews)
+        .where(
+            eq(property_reviews.propertyId, propertyId)
+        );
+
+        const reviewScore = await query;
+
+        return NextResponse.json(reviewScore.length ? reviewScore[0].numeric : null);
+    }
+    
+
+    return NextResponse.json(null);
+}
