9404a796bf2970ea99f0ffdff535b967a49e191b
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-10-26 21:31:27 +0200

    - @minor: layout change

--- components/properties/PropertyCard.tsx
+++ components/properties/PropertyCard.tsx
@@ -1,5 +1,3 @@
-
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -15,34 +13,28 @@ import {
 
 export default function PropertyCard({ property, idx }: { property: any, idx: string }) {
   return (
-    <div key={`properrty-${idx}`}>
-      <Card className="pt-0">
-        <CardHeader
-        className="relative w-full h-64 rounded-xl overflow-hidden"
-        >
-          <Image 
-            src={property.property.mainImage ? '/assets/' + property.property.mainImage : "/assets/placeholder.png"} 
-            alt="placeholder"
-            fill={true}
-            quality={85}
-            style={{ objectFit: "cover" }}
-            loading="lazy"
-          />
-        </CardHeader>
-        <CardContent>
-          <h2>{property.property.name ?? `Property ${idx + 1}`}</h2>
-          <p><strong>{property.properties.city}</strong></p>
-        </CardContent>
-        <CardFooter>
-          <CardAction>
-            <Button asChild>
-            <Link href={`/properties/property/${property.property?.slug ?? idx}`}>
-              View details
-            </Link>
-          </Button>
-          </CardAction>
-        </CardFooter>
-      </Card>
+    <div key={`property-${idx}`}>
+      <Link href={`/properties/property/${property.property?.slug ?? idx}`}>
+        <Card className="pt-0 border-gray-300 hover:border-black">
+          <CardHeader
+          className="relative w-full h-64 rounded-xl overflow-hidden"
+          >
+            <Image 
+              src={property.property.mainImage ? '/assets/' + property.property.mainImage : "/assets/placeholder.png"} 
+              alt="placeholder"
+              fill={true}
+              quality={85}
+              style={{ objectFit: "cover" }}
+              loading="lazy"
+            />
+          </CardHeader>
+          <CardContent>
+            <h2>{property.property.name ?? `Property ${idx + 1}`}</h2>
+            <p><strong>{property.properties.city}</strong></p>
+            <p>Price per night: {property.property.pricePerNight}</p>
+          </CardContent>
+        </Card>
+      </Link>
     </div>
   );
 }
