4938f7e5caf9191ed69d8bc670f9714886ea604b
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-10-28 14:01:22 +0200

    - @wip: added footer component and others

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,7 +1,9 @@
+import { Footer7 } from "./footer7";
+
 export default function Footer() {
     return (
-        <footer className="flex justify-center items-center p-4 bg-gray-800 text-white">
-            <p className="text-sm">© 2023 My App. All rights reserved.</p>
+        <footer className="p-4 text-white mx-auto">
+            <Footer7 />
         </footer>
     );
 }
