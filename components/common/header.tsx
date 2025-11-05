1f43940168339b5756e37bfb189d1921da278d00
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-11-05 20:24:53 +0200

    - @feat: light/dark logo

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,20 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
 import SignoutButton from "@/components/common/signout";
 import { ModeToggle } from "@/components/common/mode-toggle";
 import Search from "@/components/common/search";
+import { useTheme } from "next-themes";
 
 export default function Header() {
+    const { theme } = useTheme();
+
+    const logoSrc =
+    theme === "dark" ? "/assets/common/logo-dark.png" : "/assets/common/logo-light.png";
+
     return (
         <header className="p-4 bg-primary-foreground text-white">
             <div className="flex items-center container max-[1600px]:px-5 mx-auto">
                 <div className="w-1/4 self-end">
-                    <h1 className="text-2xl text-primary">
-                        <Link href="/">
-                            Home
-                        </Link>
-                    </h1>
+                    <Link href="/">
+                        <Image
+                        src={logoSrc}
+                        width={100}
+                        height={100}
+                        alt="logo"
+                        />
+                    </Link>
                 </div>
-                <div className="w-1/2">
+                <div className="w-1/2 self-end">
                     <Search />
                 </div>
                 <div className="w-1/4 flex justify-end gap-2 self-end">
