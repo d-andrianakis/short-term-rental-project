a72ca1ce8a0a875bfedfc5b0ae3cf03b77bf5ead
Dimitris Andrianakis <d.andrianakis@hotmail.com>
2025-11-26 09:19:03 +0200

    - @vercel type fix

--- app/actions/removePropertyAction.ts
+++ app/actions/removePropertyAction.ts
@@ -4,6 +4,6 @@ import { db } from "@/db/drizzle";
 import { property } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
-export async function removePropertyAction(id: number) {
+export async function removePropertyAction(id: string) {
   await db.delete(property).where(eq(property.id, id));
 }
